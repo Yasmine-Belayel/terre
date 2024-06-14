76429f852e529d5e1193acc3e675466da472b6c8
Yasmine Belayel <yas.be@hotmail.com>
2024-06-14 09:46:00 +0200

    terre06.py

--- terre06.py
+++ terre06.py
@@ -1,19 +1,5 @@
-chaine = ""
-while True:
-    chaine = input("Entrez votre texte: ")
-    chaine_alphab = chaine.isalpha()
-    if chaine_alphab:
-        inverse_chaine = chaine[::-1]
-        print(inverse_chaine) 
-        break
-    else:
-        print("Erreur! Veuillez entrer des lettres")
+import sys
 
-
-"""import sys
-
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-lettre_donnee = sys.argv[1:]
-for lettre_donnee in alphabet:
-    indice = alphabet.index(lettre_donnee)
-print(indice)"""
+arguments = sys.argv[1:]
+for i in arguments:
+    print(i[::-1])
