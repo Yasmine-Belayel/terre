6fbc18c10a4bcdf03592fd9fbf6f92692470574a
Yasmine Belayel <yas.be@hotmail.com>
2024-06-21 11:44:37 +0200

    terre09.py

--- terre09.py
+++ terre09.py
@@ -1,10 +1,15 @@
-nombre = ""
-while True:
-    nombre = input("Entrez un nombre entier positif: ")
-    if nombre.isdigit():
-        nombre_int = int(nombre)
-        resultat = nombre_int ** 0.5
-        print(f"La racine carrée de {nombre_int} est: {resultat}")
-        break
+import sys
+
+if len(sys.argv) != 2:
+    print("Erreur! Merci d'entrer uniquement UN SEUL argument")
+else:
+    try:
+        nombre = int(sys.argv[1])
+    except:
+        print("Merci d'entrer uniquement des chiffres")
     else:
-        print("erreur ! Veuillez entrer un nombre valide")
+        if nombre >= 0:
+            resultat = nombre ** 0.5
+            print(f"La racine carrée de {nombre} est: {resultat}")
+        else:
+            print("erreur ! Veuillez entrer un nombre valide")
