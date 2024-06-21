cd7e8c55cf7f046befdeb4260f83e134de32464e
Yasmine Belayel <yas.be@hotmail.com>
2024-06-21 18:06:21 +0200

    terre10.py

--- terre10.py
+++ terre10.py
@@ -3,19 +3,19 @@ import sys
 arg = sys.argv
 if len(arg) != 2:
     print("Erreur! Merci d'entrer uniquement UN SEUL argument")
-else:
-    try:
-        n = int(arg[1])
-        if n == 0 or n == 1:
-            print(f"{n} n'est pas un nombre premier")
+elif not arg[1].isdigit():
+    print("Merci d'entrer uniquement un nombre entier naturel")
+else:    
+    n = int(arg[1])
+    if n == 0 or n == 1:
+        print(f"{n} n'est pas un nombre premier")
+    else:
+        i = 2
+        racine_carree_n = n ** 0.5
+        while i <= racine_carree_n and n % i != 0:
+            i = i+1
+        if i > racine_carree_n:
+            print(f"{n} est un nombre premier")
         else:
-            i = 2
-            racine_carree_n = n ** 0.5
-            while i <= racine_carree_n and n % i != 0:
-                i = i+1
-            if i > racine_carree_n:
-                print(f"{n} est un nombre premier")
-            else:
-                print(f"{n} n'est pas un nombre premier")
-    except:
-        print("Merci d'entrer uniquement des chiffres")
+            print(f"{n} n'est pas un nombre premier")
+    
