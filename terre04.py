c4b28a6488c0e4a790fa5bbaa124f892a6bbb6b4
Yasmine Belayel <yas.be@hotmail.com>
2024-12-13 13:39:07 +0100

    mod terre04

--- terre04.py
+++ terre04.py
@@ -1,9 +1,9 @@
 import sys
 
-nombre = int(sys.argv[1])
-reste = nombre % 2
-if reste == 0:
-    print(f"le nombre {nombre} est pair")
+number = int(sys.argv[1])
+rest = number % 2
+if rest == 0:
+    print(f"le nombre {number} est pair")
 else:
-    print(f"le nombre {nombre} est impair")
+    print(f"le nombre {number} est impair")
     
