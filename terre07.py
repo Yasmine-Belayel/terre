eb773c3c4e554d6ab06297a74ca3c6d0adef70c3
Yasmine Belayel <yas.be@hotmail.com>
2024-12-26 15:08:13 +0100

    mod terre07

--- terre07.py
+++ terre07.py
@@ -1,10 +1,23 @@
 import sys
 
+# Vérification du nombre d'arguments
+if len(sys.argv) != 2:
+    print("Veuillez fournir un unique argument non numérique.")
+    sys.exit(1)
 
-if len(sys.argv) != 2:   
-    print("Erreur! Merci de saisir UN SEUL argument pour exécuter le programme")
-else:
-    if sys.argv[1].isdigit():
-        print("Erreur! Le programme accepte uniquement une chaîne de caractères")
-    else:   
-        print(f"Votre chaîne de caractères contient {len(sys.argv[1])} caractères")
+argument = sys.argv[1]
+
+# Vérification si l'argument est un nombre
+if argument.isdigit() or (argument[0] == '-' and argument[1:].isdigit()):
+    print("ERREUR : l'argument ne doit pas être un nombre.")
+    sys.exit(1)
+
+# Initialiser un compteur pour la longueur
+count = 0
+
+# Boucle pour compter les caractères dans l'argument
+for char in argument:
+    count += 1  # Incrémenter le compteur pour chaque caractère
+
+# Affichage de la taille de la chaîne
+print(f"La taille de la chaîne est : {count}")
