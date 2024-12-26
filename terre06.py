62f1913b08ee0efd984a3f83769164cd966ee1c8
Yasmine Belayel <yas.be@hotmail.com>
2024-12-26 14:44:08 +0100

    mod terre06

--- terre06.py
+++ terre06.py
@@ -1,5 +1,23 @@
 import sys
 
-arguments = sys.argv[1:]
-for i in arguments:
-    print(i[::-1])
+# Vérification du nombre d'arguments
+if len(sys.argv) != 2:
+    print("Veuillez fournir un unique argument non numérique.")
+    sys.exit(1)
+
+argument = sys.argv[1]
+
+# Vérification si l'argument est un nombre
+if argument.isdigit() or (argument[0] == '-' and argument[1:].isdigit()):
+    print("ERREUR : l'argument ne doit pas être un nombre.")
+    sys.exit(1)
+
+# Initialisation d'une chaîne pour stocker le résultat
+reversed_argument = ""
+
+# Boucle pour inverser l'argument
+for char in argument:
+    reversed_argument = char + reversed_argument
+
+# Affichage du résultat
+print(reversed_argument)
