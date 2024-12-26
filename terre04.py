7f3adea0cd0fd8e2837fd3ba142c0ebe412ff6ad
Yasmine Belayel <yas.be@hotmail.com>
2024-12-26 13:59:58 +0100

    mod terre04

--- terre04.py
+++ terre04.py
@@ -1,13 +1,22 @@
 import sys
 
-number = int(sys.argv[1])
-if len(number) != 2:   
-    print("Erreur! Merci de saisir UN SEUL argument pour exécuter le programme")
-elif sys.argv[1].isalpha():
-    print("Erreur! Le programme accepte uniquement des chiffres")
+if len(sys.argv) < 2:
+    print("Veuillez fournir un nombre en argument")
+    sys.exit(1)
+
+
+argument = sys.argv[1]
+
+
+if argument.isdigit() or (argument[0] == '-' and argument[1:].isdigit()):
+    number = int(argument)  
+else:
+    print("Veuillez entrer un nombre valide")
+    sys.exit(1)
+
+
 rest = number % 2
 if rest == 0:
     print(f"le nombre {number} est pair")
 else:
     print(f"le nombre {number} est impair")
-    
